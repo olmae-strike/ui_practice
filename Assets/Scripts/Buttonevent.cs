6ee80d66c70183063d8732f4bdacd607abe33d5f
olmae-strike <65939875+olmae-strike@users.noreply.github.com>
2020-11-23 22:51:52 +0900

    third commit

---
+++ Assets/Scripts/Buttonevent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Buttonevent : MonoBehaviour
+{
+   // [HideInInspector]
+    public Button btn;
+
+    public InputField input;
+
+    //[HideInInspector]
+    public Text text;
+
+    //public User user;
+    void Start()
+    {
+        btn.onClick.AddListener(() => {
+            text.text = "zinnotech";
+        });
+        input.onValueChanged.AddListener((temp) =>  {
+            Debug.Log($"message {temp}");
+        });
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+public class TestClass : MonoBehaviour
+{
+    public TestClass()
+    {
+        Buttonevent be = new Buttonevent();
+        //be.
+    }
+}
+
+[Serializable]
+public class User
+{
+    public string Name;
+    public string Number;
+}
+
